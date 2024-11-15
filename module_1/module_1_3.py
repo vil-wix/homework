f716020af397723d15226d02102c772bc353829e
Виолетта <vita.vasileva.2005@inbox.ru>
2024-11-15 20:09:38 +0600

    все домашние задания выполненные на 15.11.24

---
+++ module_1/module_1_3.py
@@ -0,0 +1,8 @@
+name='Виолетта'
+print('Name:', name)
+age=19
+print('Age:', age)
+age=age+19
+print('New Age:', age)
+is_student=True
+print('Is_student:', is_student)
---
+++ module_1/module_1_4.py
@@ -0,0 +1,5 @@
+homework= 12
+hours_spent= 1.5
+name= 'Python'
+time= hours_spent / homework
+print('Курс:' + str(name) + ',', 'всего задач:' + str(homework) + ',', 'затрачено часов:' + str(hours_spent) + ',', 'среднее время выполнения', time, 'часа.')
