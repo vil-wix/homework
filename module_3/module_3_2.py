94ef01f351f849b9143528b6650e71a99321bdd9
Виолетта <vita.vasileva.2005@inbox.ru>
2024-11-22 22:21:25 +0600

    все домашние задания выполненные на 15.11.24

--- module_3/module_3_2.py
+++ module_3/module_3_2.py
@@ -1,12 +1,12 @@
-variants = (".com", ".ru", ".net", ".gmail")
+variants = (".com", ".ru", ".net")
 
 
 def send_email(sms, recipient, *, sender='university.help@gmail.com'):
-    if '@' not in recipient and '@' not in sender or sender.endswith(variants) and  recipient.endswith(variants):
+    if '@' not in recipient and '@' not in sender and not sender.endswith(variants) and not recipient.endswith(variants):
         print('Невозможно отправить письмо с адреса ',sender,' на адрес' + recipient)
     elif sender == recipient:
         print('Нельзя отправить письмо самому себе!')
-    elif sender == sender:
+    elif sender == 'university.help@gmail.com':
         print('Письмо успешно отправлено с адреса ', sender, ' на адрес ' + recipient)
     else:
         print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ' + sender + ' на адрес' + recipient)
