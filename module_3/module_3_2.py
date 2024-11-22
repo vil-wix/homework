f3c60e8973022fa2dd320b051ff84bfd995871bd
Виолетта <vita.vasileva.2005@inbox.ru>
2024-11-22 22:16:09 +0600

    все домашние задания выполненные на 15.11.24

--- module_3/module_3_2.py
+++ module_3/module_3_2.py
@@ -14,4 +14,4 @@ def send_email(sms, recipient, *, sender='university.help@gmail.com'):
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+send_email('Напоминаю самому себе о вебинаре',  'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
