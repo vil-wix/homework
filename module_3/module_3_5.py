fd7036c7111f4ccf283583624fe20ffce802e1d4
Виолетта <vita.vasileva.2005@inbox.ru>
2024-12-05 12:25:29 +0600

    все домашние задания выполненные на 15.11.24

--- module_3/module_3_5.py
+++ module_3/module_3_5.py
@@ -1,9 +1,9 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
-    if str_number.endswith('0'):
+    while str_number.endswith('0'):
         str_number=str_number[:len(str_number)-1]
-    elif len(str_number) > 1:
+    if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
