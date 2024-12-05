043e928ffc9d00c39a044c0f4f9d34c8a4f9f36d
Виолетта <vita.vasileva.2005@inbox.ru>
2024-12-05 12:46:31 +0600

    все домашние задания выполненные на 15.11.24

---
+++ module_3/module_3_hard.py
@@ -0,0 +1,15 @@
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+def calculate_structure_sum(data_structure):
+
+
+
+
+result = calculate_structure_sum(data_structure)
+print(result)
+
