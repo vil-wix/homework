7a88fab1e421ecfac35c85d1be21637287fa6664
Виолетта <vita.vasileva.2005@inbox.ru>
2024-12-09 13:38:05 +0600

    все домашние задания выполненные на 15.11.24

--- module_3/module_3_4.py
+++ module_3/module_3_4.py
@@ -1,15 +1,12 @@
-import re
-
-
 def single_root_words(root_word, *other_words):
     same_words = []
-    root_word=root_word.lower()
+    root_word = root_word.lower()
     for i in other_words:
-        if re.search(root_word, i):
+        if root_word in i.lower() or i.lower() in root_word:
             same_words.append(i)
-        elif re.search(i,root_word):
-            same_words.append(i)
+
     return same_words
+
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
