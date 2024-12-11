7e9cdc441d82819cf24a7816326e18f914d4dcd5
Виолетта <vita.vasileva.2005@inbox.ru>
2024-12-11 16:49:17 +0600

    все домашние задания выполненные на 15.11.24

--- module_3/module_3_hard.py
+++ module_3/module_3_hard.py
@@ -1,15 +1,27 @@
-data_structure = [
-[1, 2, 3],
-{'a': 4, 'b': 5},
-(6, {'cube': 7, 'drum': 8}),
-"Hello",
-((), [{(2, 'Urban', ('Urban2', 35))}])
-]
 def calculate_structure_sum(data_structure):
+    sum = 0
+    if isinstance(data_structure, (list, tuple, set)):
+        for item in data_structure:
+            sum += calculate_structure_sum(item)
+    elif isinstance(data_structure, int):
+        sum += data_structure
+    elif isinstance(data_structure, str):
+        sum += len(data_structure)
+    elif isinstance(data_structure, dict):
+        for key, array in data_structure.items():
+            sum += calculate_structure_sum(key)
+            sum += calculate_structure_sum(array)
+
+    return sum
 
 
-
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
 
 result = calculate_structure_sum(data_structure)
 print(result)
-
