1e9eb1e9efa35a1aa8fea9b4a8f0aef8a9668d42
Виолетта <vita.vasileva.2005@inbox.ru>
2025-01-13 23:49:30 +0600

    все домашние задания выполненные на 15.11.24

---
+++ module_5/module_5_3.py
@@ -0,0 +1,64 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        num = 0
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print(f'Такого этажа не существует')
+        else:
+            for num in range(new_floor):
+                print(num + 1)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название:'{self.name}', кол-во этажей:'{self.number_of_floors}'"
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, other):
+        return self.number_of_floors
+
+    def __radd__(self, other):
+        return self.number_of_floors
+
+    def __iadd__(self, other):
+        return self.number_of_floors
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+print(h1)
+print(h2)
+print(h1 == h2)
+h1 = h1 + 10
+print(h1)
+print(h1 == h2)
+h1 += 10
+print(h1)
+h2 = 10 + h2
+print(h2)
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h2)
