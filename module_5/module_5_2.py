7502c325e2aba6e1c487498ca54be5f7360db21d
Виолетта <vita.vasileva.2005@inbox.ru>
2025-01-13 23:06:37 +0600

    все домашние задания выполненные на 15.11.24

---
+++ module_5/module_5_2.py
@@ -0,0 +1,26 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        num = 0
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print(f'Такого этажа не существует')
+        else:
+            for num in range(new_floor):
+                print(num + 1)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return "Название:",str({ self.name}), ', кол-во этажей:', str({self.number_of_floors})
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+print(h1)
+print(h2)
+print(len(h1))
+print(len(h2))
